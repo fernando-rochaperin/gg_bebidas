0bd45875abac5f5ecab2ceb12c7f34367c7eab65
fernando-rochaperin <rochaperin2025@gmail.com>
2025-05-09 11:21:03 -0300

    Update script.js
    Adicionado o campo uber

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const googleMapsBtn = document.getElementById('googleMapsBtn');
 const wazeBtn = document.getElementById('wazeBtn');
 const uberBtn = document.getElementById('uberBtn'); // Assuming you added an Uber button with this ID
 
+const destinationLatitude = -12.9807068;
+const destinationLongitude = -38.4313148;
+
+
 googleMapsBtn.addEventListener('click', () => {
   window.location.href = 'https://www.google.com/maps/search/?api=1&query=-12.9807068,-38.4313148';
 });
@@ -14,5 +18,5 @@ wazeBtn.addEventListener('click', () => {
 // This is a placeholder for Uber functionality.
 uberBtn.addEventListener('click', () => {
   // Replace with actual Uber link or app invocation
-  window.location.href = 'https://m.uber.com/looking'; 
+  window.location.href = 'https://m.uber.com/looking?action=setPickup&dropoff[latitude]=${destinationLatitude}&dropoff[longitude]=${destinationLongitude}'; 
 });
